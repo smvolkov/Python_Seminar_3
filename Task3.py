cbe8b41d66d41670c6e311a73a640f18ed2cc052
Semion Volkov <semen_volkov@mail.ru>
2023-01-11 14:57:43 +0500

    Task3 updated

--- Task3.py
+++ Task3.py
@@ -9,6 +9,7 @@ arr = [1.1, 1.2, 3.1, 5, 10.01]
 arr_1 = []
 
 for i in arr:
-    arr_1.append(i - int(i))
+    if i - int(i) != 0:
+        arr_1.append(i - int(i))
     
-print(max(arr_1) - min(arr_1))
+print(round(max(arr_1) - min(arr_1), 2))
